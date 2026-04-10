4ad63f2226d3a882100acdc5341dd98dc5a538d2
Seth <sethyuan@gmail.com>
2026-04-10 10:19:40 +0800

    fix: make query example complete

--- examples/query_error_logs.sh
+++ examples/query_error_logs.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-orcanote query_blocks --repo demo-repo --input '{"description":{"q":{"kind":100,"conditions":[{"kind":4,"name":"Log","properties":[{"name":"App","op":1,"value":"Orca Note"},{"name":"Level","op":1,"value":"Error"}]}]},"page":1,"pageSize":100}}' --json
+log_ids=$(
+  orcanote query_blocks --repo demo-repo --input '{"description":{"q":{"kind":100,"conditions":[{"kind":4,"name":"Log","properties":[{"name":"App","op":1,"value":"Orca Note"},{"name":"Level","op":1,"value":"Error"}]}]},"page":1,"pageSize":100}}' --json | jq -r -c '.resultIds'
+)
+
+orcanote get_blocks_text --repo demo-repo --input "{\"blockIds\": $log_ids}"
